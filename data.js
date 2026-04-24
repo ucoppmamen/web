ef32dde891e15800c2bc1f4e4e65cc4f99d54de3
yusuf kurniawan <ysuf1077@gmail.com>
2026-04-24 09:22:37 +0700

    Upload Github Pertamaku

---
+++ data.js
@@ -0,0 +1,21 @@
+const databaseProyek = {
+    "sistem_pos": {
+        judul: "SISTEM POST",
+        keahlian: ["JavaScript","PHP"],
+        gambar: "https://darmawan.capital/wp-content/uploads/2021/03/indodax-logo.png",
+        latar: "ini adalah proyek magang saya membuat sistem POS untuk PT INDODAX",
+        tantangan: "kebutuhan dari PTnya sangat rumit",
+        pelajaran: "Saya mendapatkan pelajran bahwa sistem pos PT INDODAX ideal skala kecil",
+        link: ""
+    },
+"sistem_pos_xyz" : {
+        judul: "SISTEM POST",
+        keahlian: ["JavaScript","PHP"],
+        gambar: "https://darmawan.capital/wp-content/uploads/2021/03/indodax-logo.png",
+        latar: "ini adalah proyek magang saya membuat sistem POS untuk PT INDODAX",
+        tantangan: "kebutuhan dari PTnya sangat rumit",
+        pelajaran: "Saya mendapatkan pelajran bahwa sistem pos PT INDODAX ideal skala kecil",
+        link: ""
+    },
+};
+
